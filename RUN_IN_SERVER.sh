1d561f3973d9db8a46c3eef3b959d80962325186
Bernardo Estrada <estradaf.bernardo@gmail.com>
2022-11-16 16:57:20 -0600

    Add flags to script (for resume/force)

--- RUN_IN_SERVER.sh
+++ RUN_IN_SERVER.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-if (($# != 1))
+if (($# < 1))
 then
-  echo "Usage: $0 [run-id]"
+  echo "Usage: $0 [run-id] [other flags]"
 else
+id=$1
+shift
 #screen \
 mlagents-learn \
 Assets/Dino.yaml \
 --env=./build/LINUX_SERVER_DINO/dino-ml-training.x86_64 \
 --num-envs=2 \
 --num-areas=30 \
---run-id={$1} \
+--run-id={$id} \
 --no-graphics \
---torch-device=cuda
+--torch-device=cuda \
+$@
 fi
