a8a4f74e13de4b135011dbd740e31a05bf06955f
Bernardo Estrada <estradaf.bernardo@gmail.com>
2022-11-16 18:14:14 -0600

    Update .gitignore and Fetch Results

--- FETCH_RESULTS.sh
+++ FETCH_RESULTS.sh
@@ -1,1 +1,2 @@
-scp -rp A01704320@10.25.24.6:/home/alumnos/A01704320/MLAgents/dino/results ./.
+scp -rp A01704320@10.25.24.6:/home/alumnos/A01704320/MLAgents/dino/results ./. &&
+scp -rp A01704320@10.25.24.6:/home/alumnos/A01704320/MLAgents/dino/Assets/Demonstrations/Learning ./Assets/Demonstrations/
